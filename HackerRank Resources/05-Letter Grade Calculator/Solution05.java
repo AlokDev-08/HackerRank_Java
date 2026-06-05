fe3a50485e5dbd08d1ae6953ce9cf5b137f10247
Alok Dev <mk.alok806@gmail.com>
2026-06-05 14:10:53 +0530

    Implement letter grade calculator based on marks

--- HackerRank Resources/05-Letter Grade Calculator/Solution05.java
+++ HackerRank Resources/05-Letter Grade Calculator/Solution05.java
@@ -1,2 +1,25 @@
-public class Solution05 {
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int marks = sc.nextInt();
+        
+        if(marks<=100 && marks>=90){
+            System.out.println("A");
+        }
+        else if (marks<=89 && marks>=80){
+            System.out.println("B");
+        }
+        else if (marks<=79 && marks>=70){
+            System.out.println("C");
+        }
+        else if (marks<70){
+            System.out.println("F");
+        }
+        sc.close();
+    }
 }
