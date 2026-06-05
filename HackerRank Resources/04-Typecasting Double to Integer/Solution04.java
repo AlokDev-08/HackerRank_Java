85f484a7dab178fdc07d0675863dd317f01b30f7
Alok Dev <mk.alok806@gmail.com>
2026-06-05 14:03:20 +0530

    Update Solution04.java

--- HackerRank Resources/04-Typecasting Double to Integer/Solution04.java
+++ HackerRank Resources/04-Typecasting Double to Integer/Solution04.java
@@ -1,2 +1,18 @@
-public class Solution04 {
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double n = sc.nextDouble();
+        
+        int result =(int)n;
+        
+        System.out.println(result);
+        
+        sc.close();
+        
+    }
 }
