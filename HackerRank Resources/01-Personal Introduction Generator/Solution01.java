12aecda0d7859ca845e0b86a3fe7a686aa995176
Alok Dev <mk.alok806@gmail.com>
2026-06-05 13:40:00 +0530

    Update Solution01.java

--- HackerRank Resources/01-Personal Introduction Generator/Solution01.java
+++ HackerRank Resources/01-Personal Introduction Generator/Solution01.java
@@ -1,2 +1,18 @@
-public class Solution01 {
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fName = sc.nextLine();
+        String lName = sc.nextLine();
+        int age = sc.nextInt();
+        double height = sc.nextDouble();
+        boolean likes = sc.nextBoolean();
+        
+        
+        System.out.println("Hello, my name is "+fName+" "+lName+". I am "+age+" years old, "
+        +height+" meters tall, and it is "+likes+" that I like programming!");
+    }
 }
