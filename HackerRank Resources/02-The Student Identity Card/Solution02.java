aa77bc4fc458f66a6c0ea0e8cfdefb7d7903da9c
Alok Dev <mk.alok806@gmail.com>
2026-06-05 13:27:00 +0530

    Add student information display functionality
    Implemented a solution to read and display student information.

--- HackerRank Resources/02-The Student Identity Card/Solution02.java
+++ HackerRank Resources/02-The Student Identity Card/Solution02.java
@@ -1,2 +1,19 @@
-public class Solution02 {
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int erp = sc.nextInt();
+        int age = sc.nextInt();
+        sc.nextLine();
+        String name = sc.nextLine();
+        String address = sc.nextLine();
+        
+        System.out.println("ERP: "+erp);
+        System.out.println("Age: "+age);
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+address);
+    }
 }
